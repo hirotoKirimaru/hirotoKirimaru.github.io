5fb26784a8802b34681377b84245395f73ced537
nainaistar <nainaistar@gmail.com>
2020-05-06 21:17:16 +0900

    [Add] ts化したり、レイアウトを自分好みにしたり。

--- src/components/core/View.vue
+++ src/components/core/View.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
-    <welcome />
-
-    <recent-projects />
-
-    <services />
+    <!--    <welcome />-->
+    <!--    <recent-projects />-->
+    <!--    <services />-->
 
     <about-me />
+    <career />
 
     <get-in-touch />
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'CoreView',
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
 
+  @Component({
     components: {
-      AboutMe: () => import('@/components/AboutMe'),
-      GetInTouch: () => import('@/components/GetInTouch'),
-      RecentProjects: () => import('@/components/RecentProjects'),
-      Services: () => import('@/components/Services'),
-      Welcome: () => import('@/components/Welcome'),
+      AboutMe: () => import('@/components/AboutMe.vue'),
+      GetInTouch: () => import('@/components/GetInTouch.vue'),
+      RecentProjects: () => import('@/components/RecentProjects.vue'),
+      Services: () => import('@/components/Services.vue'),
+      Welcome: () => import('@/components/Welcome.vue'),
+      Career: () => import('@/components/Career.vue'),
     },
-  }
+  })
+  export default class CoreView extends Vue {}
 </script>
